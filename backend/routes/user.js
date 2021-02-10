f727e9bbf63732ddf6ddbcbb3a813ba778f0e52a
Ghassan ALNomani <alnomanif15@gmail.com>
2021-02-10 17:31:14 +0300

    create routes comment,place,user. finished server.js file

---
+++ backend/routes/user.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../models/user")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
