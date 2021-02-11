41f3323233e1a50e8cb5233202b3822a0c7522d5
huda <huda.badahdooh@hotmail.com>
2021-02-11 09:26:35 +0300

    ..

---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+
+
+
+export default function home() {
+
+    const [places , setPlace] = useState([])
+    const [selectPlaces , setSelectPlaces] = useState([])
+
+
+
+
+    useEffect(() => {
+        Axios
+        .get('', places)
+        .then(res =>{     
+            setPlace(res.data);
+             setSelectPlace(res.data)
+         
+        })
+    }, [])
+
+    const allplaces = selectPlaces.map(place =>{
+
+        return <PlaceCard place= {place} />
+      }) 
+
+
+
+
+
+
+    return (
+        <div>
+              <div className="logo relative centerText">
+              
+                <br />
+
+                <div className="placeContainer">
+
+                {allplaces}
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
