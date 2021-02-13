fc808f2eb68bac604a1fd5919a5f27f41e5a0a8a
Ghassan ALNomani <alnomanif15@gmail.com>
2021-02-13 22:08:51 +0300

    userprofile @Ahlam

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Landing from './components/Landing';
-// import AuthRoute from"./components/AuthRoute"
+
 import Create from './components/Create';
 
 import NavBar from './components/NavBar';
-
+import EditProfile from './components/EditProfile';
 import Home from "./components/Home";
 import ShowPage from './components/ShowPage';
 import AuthRoute from './components/AuthRoute';
@@ -25,41 +25,7 @@ function App() {
 
   const [auth, setAuth] = useState({ currentUser: null, isLoggedIn: false });
   const [userProfile , setUserProfile] = useState({})
-  // const userLogin = () => {
-  //   if (localStorage.jwtToken) {
-  //     const jwtToken = localStorage.jwtToken;
-  //     const currentUser = jwt_decode(jwtToken, "SECRET").user;
-  //     const currentPlace = jwt_decode(jwtToken, "SECRET").place;
-  //     const currentDataUser = jwt_decode(jwtToken, "SECRET").user;
-  //     setAuth({ currentUser, isLoggedIn: true });
-  //     setUserData({ currentDataUser });
-  //     getProfile(currentUser);
-  //     // getBook(currentPlace)
-
-  //   } else {
-  //     setAuth({ currentUser: null, isLoggedIn: false });
-  //   }
-
-  //   // setDataloading(true)
-  //   setDataloaded(true)
-  //   console.log("The current User is: ", auth.currentUser);
-  //   console.log("The current DATA User  ", userData.currentDataUser);
-
-  // };
-  // const getProfile = async (currentUser) => {
-  //   const { data: { user } } = await axios.get(`${API_URL}/api/users/profile/${currentUser._id}`)
-  //   console.log('Loaded user profile: ', user)
-  //   setUserProfile(user)
-  // }
-
-
-  // useEffect(userLogin, []);
-
-  // useEffect(() => {
-  //   if (userProfile.name) {
-  //     setDataloaded(true)
-  //   }
-  // }, [userProfile])
+  
 
   const userLogin = () => {
     if (localStorage.jwtToken) {
@@ -110,7 +76,9 @@ function App() {
                 <ShowPage/>
                 
               </Route>
-
+              <Route path="/editprofile">
+                <EditProfile />
+              </Route> 
               <Route path="/create">
                   <Create />
               </Route>
