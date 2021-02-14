a119855434e81eeb0586c8eca4e922d26cb5feb3
Ghassan ALNomani <alnomanif15@gmail.com>
2021-02-14 16:53:31 +0300

    fix

---
+++ frontend/frontend/src/components/action/NavBar.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBIcon } from 'mdbreact';
+import { useHistory } from 'react-router-dom';
+export default function NavBar(props) {
+    const bgPink = { backgroundColor: '' }
+    const history = useHistory();
+
+
+
+
+    const logOut = () => {
+        console.log("Logging Out!");
+        localStorage.removeItem("jwtToken");
+        props.loginCallback();
+        history.push('/home');
+    }
+
+
+
+
+    return (
+        <div>
+            <header>
+                <MDBNavbar style={bgPink} dark expand="md"  >
+{/*                 scrolling fixed="top"
+ */}                    <MDBNavbarBrand href="/">
+                        <strong></strong>
+                    </MDBNavbarBrand>
+                    <MDBNavbarNav left>
+                        <MDBNavItem active>
+                            <MDBNavLink to="/home"><MDBIcon icon="home" /> Home</MDBNavLink>
+                        </MDBNavItem>
+                        <MDBNavItem>
+                            <MDBNavLink to="/aboutus"> <MDBIcon icon="info" /> About us</MDBNavLink>
+                        </MDBNavItem>
+                    </MDBNavbarNav>
+                    {!props.isLoggedIn ? <> 
+                        
+                        <MDBNavbarNav right>
+                                <MDBNavItem>
+                                    <MDBNavLink to="/signup"><MDBIcon icon="user-plus" /> Signup</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="/login"> <MDBIcon icon="sign-in-alt" />Login</MDBNavLink>
+                                </MDBNavItem>
+                            </MDBNavbarNav>
+                           </> : <>
+                            <MDBNavbarNav right>
+                                <MDBNavItem>
+                                    <MDBNavLink to="/profile"><MDBIcon far icon="user" /> Profile</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem onClick={() => logOut()}>
+                                    <MDBNavLink to="/logout" > <MDBIcon icon="sign-out-alt" />Logout</MDBNavLink>
+                                </MDBNavItem>
+                            </MDBNavbarNav>
+                            </>}
+                        
+                    
+                </MDBNavbar>
+                
+            </header>
+            
+        </div>
+        
+    )
+}
---
+++ frontend/frontend/src/components/pages/Footer.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBRow } from "mdbreact";
+
+export const Footer = () => {
+    return (
+        <>
+            <div className="footer">
+                    <footer>&copy; Copyright 2021 Team Final-Battle</footer>
+            </div>
+        </>
+    )
+}
